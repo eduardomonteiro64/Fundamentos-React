8e5f60ea8570bc40090ea2c17dec3135c62db70b
Eduardo Monteiro <eduardomonteiro64@gmail.com>
2019-06-06 08:27:31 -0300

    primeiro commit

---
+++ src/componentes/MultiElementos.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default props => [<h1>Parte 1</h1>, <h2>Parte 2</h2>];
+
+// Solução 01. Mais usada.
+// export default props => (
+//     <div>
+//       <h1>Parte 1</h1>
+//       <h2>Parte 2</h2>
+//     </div>
+//   );
+
+// export default props => (
+//   <React.Fragment>
+//     <h1>Parte 1</h1>
+//     <h2>Parte 2</h2>
+//   </React.Fragment>
+// );
--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+// import PrimeiroComponente from "./componentes/PrimeiroComponente";
+// import A, { CompB as B } from "./componentes/DoisComponentes";
+// import MultiElementos from "./componentes/MultiElementos";
+// import FamiliaSilva from "./componentes/FamiliaSilva";
+// import Familia from "./componentes/Familia";
+// import Membro from "./componentes/Membro";
+// import ComponenteComFuncao from "./componentes/ComponenteComFuncao";
+// import Pai from "./componentes/Pai";
+// import ComponenteClasse from "./componentes/ComponenteClasse";
+// import Contador from "./componentes/Contador";
+import Hook from "./componentes/Hook";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const elemento = document.getElementById("root");
+ReactDOM.render(
+  <div>
+    <Hook />
+    {/* <Contador numeroInicial={100} /> */}
+    {/* <ComponenteClasse valor="Sou um componente de classe!" /> */}
+    {/* <Pai /> */}
+    {/* <ComponenteComFuncao />     */}
+    {/* <Familia sobrenome="Pereira ">
+      <Membro nome="Andre " />
+      <Membro nome="Mariana " />
+    </Familia> */}
+    {/* <FamiliaSilva /> */}
+    {/* <MultiElementos />*/}
+    {/* <A valor="Olá eu sou A!" />
+    <B valor="B na área!" /> */}
+    {/* <PrimeiroComponente valor="Bom dia!" aBcD={123} />  */}
+  </div>,
+  elemento
+);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// const jsx = <h1>Olá React!</h1>;
+// ReactDOM.render(jsx, elemento);
